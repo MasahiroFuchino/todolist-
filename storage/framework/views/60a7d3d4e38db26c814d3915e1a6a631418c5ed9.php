977e5faa36dcc38ee371ec98e6f00e8e1d4accea
MasahiroFuchino <96112545+MasahiroFuchino@users.noreply.github.com>
2022-02-05 12:34:52 +0900

    Add files via upload

---
+++ storage/framework/views/60a7d3d4e38db26c814d3915e1a6a631418c5ed9.php
@@ -0,0 +1,4 @@
+<a href="<?php echo e(route('password.reset', ['token' => $token])); ?>">
+  パスワード再設定リンク
+</a>
+<?php /**PATH /home/vagrant/Code/Laravel/resources/views/mail/password-reset.blade.php ENDPATH**/ ?>
